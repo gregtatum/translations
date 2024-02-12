d4bfcccdcf4c11aeb78f063c5165d3136a905a73
Greg Tatum <tatum.creative@gmail.com>
2024-02-12 08:59:05 -0600

    WIP?

--- pipeline/data/importers/corpus/url.py
+++ pipeline/data/importers/corpus/url.py
@@ -4,15 +4,17 @@ Import data from a Google Cloud storage bucket.
 
 Example usage:
 
-pipeline/data/importers/corpus/bucket.py \
-  en                                                       `# src`
-  ru                                                       `# trg`
-  $artifacts/releng-translations-dev_data_custom-en-ru_zip `# output_prefix`
-  releng-translations-dev/data/custom-en-ru.zip            `# name`
+pipeline/data/importers/corpus/url.py                                                       \\
+  en                                                                      `# src`           \\
+  ru                                                                      `# trg`           \\
+  artifacts/releng-translations-dev_data_custom-en-ru_zip                 `# output_prefix` \\
+  http://storage.google.com/releng-translations-dev/data/custom-en-ru.zip `# url`
 """
 
 import argparse
 import re
+
+import requests
 
 from pipeline.utils.downloads import google_cloud_storage
 
@@ -29,6 +31,13 @@ def parse_name(name: str):
     return bucket_name, file_path
 
 
+def download(url: str, destination: str):
+    response = requests.get(url, stream=True)
+    with open(destination, "wb") as f:
+        for chunk in response.iter_content(chunk_size=1024**2):
+            f.write(chunk)
+
+
 def main() -> None:
     parser = argparse.ArgumentParser(
         description=__doc__,
@@ -37,7 +46,7 @@ def main() -> None:
     parser.add_argument("src", type=str)
     parser.add_argument("trg", type=str)
     parser.add_argument("output_prefix", type=str)
-    parser.add_argument("name", type=str)
+    parser.add_argument("url", type=str)
 
     args = parser.parse_args()
 
@@ -48,22 +57,19 @@ def main() -> None:
     print("output_prefix", args.output_prefix)
     print("name", args.name)
     print("bucket_name", bucket_name)
-    print("file_path", f"{file_path}.{args.src}.zst")
-    print("file_path", f"{file_path}.{args.trg}.zst")
 
-    src_file = f"{file_path}.{args.src}.zst"
-    trg_file = f"{file_path}.{args.trg}.zst"
+    src_file = args.src.replace("{lang}", args.src)
+    trg_file = args.trg.replace("{lang}", args.trg)
     src_dest = f"{args.output_prefix}.{args.src}.zst"
     trg_dest = f"{args.output_prefix}.{args.trg}.zst"
 
-    client = google_cloud_storage.Client.create_anonymous_client()
-    bucket = client.bucket(bucket_name)
+    print(f"src_dest: {src_dest}")
+    print(f"trg_dest: {trg_dest}")
 
-    print("Bucket:", bucket_name)
     print("Downloading:", src_file)
-    bucket.blob(src_file).download_to_filename(src_dest)
+    download(src_file, src_dest)
     print("Downloading:", src_file)
-    bucket.blob(trg_file).download_to_filename(trg_dest)
+    download(trg_file, trg_dest)
 
 
 if __name__ == "__main__":
