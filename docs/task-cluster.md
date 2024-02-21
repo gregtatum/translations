0a4cf071b6896aeb983f861c9b057321ff878e0f
Greg Tatum <tatum.creative@gmail.com>
2024-02-21 16:19:30 -0600

    WIP alignments work

--- docs/task-cluster.md
+++ docs/task-cluster.md
@@ -8,23 +8,23 @@ parent: Orchestrators
 # Taskcluster
 
 [Taskcluster](https://taskcluster.net/) is a Mozilla task execution framework. It powers Firefox CI and
-provides access to the hybrid cloud workers (GCP or on-prem) 
-which increases scalability and observability compared to [Snakemake](snakemake.md). 
+provides access to the hybrid cloud workers (GCP or on-prem)
+which increases scalability and observability compared to [Snakemake](snakemake.md).
 
-We use [Taskcluster taskgraph](https://taskcluster-taskgraph.readthedocs.io/en/latest/) to define the DAG 
+We use [Taskcluster taskgraph](https://taskcluster-taskgraph.readthedocs.io/en/latest/) to define the DAG
 (Directly Acyclic Graph) of the pipeline steps.
 
 ## Running training
 
-1. Create a new branch in the git repo and push. 
+1. Create a new branch in the git repo and push.
    It is useful to experiment with code and also not to get the caches invalidated if you need to restart training and some new changes were landed in the main branch.
-    
+
 2. Go to Github CI for the commit you want to run training for and find a Decision Task
 
 ![Find CI](img/github-tc-ci.png)
 
-3. Go to CI and press "View task in Taskcluster". 
-   Make sure you are authenticated in the TC interface. It is required to run tasks. 
+3. Go to CI and press "View task in Taskcluster".
+   Make sure you are authenticated in the TC interface. It is required to run tasks.
    However, already running tasks can be viewed without authentication.
 
 ![Go to TC](img/github-view-task.png)
@@ -37,7 +37,7 @@ We use [Taskcluster taskgraph](https://taskcluster-taskgraph.readthedocs.io/en/l
 
 ![Choose action](img/tc-train-action.png)
 
-6. Copy a config prepared in advance and press "train". See the example TC config [here](https://github.com/mozilla/firefox-translations-training/tree/main/configs/tc.prod.yml). 
+6. Copy a config prepared in advance and press "train". See the example TC config [here](https://github.com/mozilla/firefox-translations-training/tree/main/configs/tc.prod.yml).
    You can find directions on how to configure training in the [Model training guide](training-guide.md).
 
 ![Start training](img/tc-train.png)
@@ -49,10 +49,10 @@ We use [Taskcluster taskgraph](https://taskcluster-taskgraph.readthedocs.io/en/l
 ![Action tasks](img/tc-train-action-tasks.png)
 
 2. Press any task. Here you can look at the logs and artifacts produced by the task.
-   
+
 ![A task](img/tc-task.png)
 
-3. Navigate to a parent Task Group again (it is a different one than for the Train Action). 
+3. Navigate to a parent Task Group again (it is a different one than for the Train Action).
    Here you can see all the scheduled tasks in a more convenient interface with filtering.
 
 ![All tasks](img/tc-all-tasks.png)
@@ -77,12 +77,12 @@ It is possible to manually cancel a task with the Cancel task action.
 
 ![Cancel](img/tc-cancel.png)
 
-After the fixes were implemented, push again and restart the pipeline with the same procedure 
+After the fixes were implemented, push again and restart the pipeline with the same procedure
 as described in the "Running training" section.
 
 ### Caching
 
-Some steps might be already cached from the previous run depending on the fixes. 
+Some steps might be already cached from the previous run depending on the fixes.
 For example if only a config setting that affects the last task was changed,
 or if nothing changed at all the pipeline might restart from the failed/cancelled step.
 
@@ -136,7 +136,7 @@ To start an interactive task, follow these steps:
 
 4. Click the "Interactive" toggle in the top left
 
-5. Reduce the maxRunTime to a best guess at how long you'll need the task and worker running for. (We pay for every minute a worker runs - so they should not be kept running, eg: overnight.)
+5. Reduce the number of seconds in the maxRunTime to a best guess at how long you'll need the task and worker running for. (We pay for every minute a worker runs - so they should not be kept running, eg: overnight.)
 
 6. Adjust the payload to simply run bash and sleep (instead of a full pipeline step). For docker-worker tasks use something like:
 ```
--- pipeline/alignment/generate-alignment-and-shortlist.sh
+++ pipeline/alignment/generate-alignment-and-shortlist.sh
@@ -34,72 +34,141 @@ corpus_src="${corpus_prefix}.${SRC}.${ARTIFACT_EXT}"
 corpus_trg="${corpus_prefix}.${TRG}.${ARTIFACT_EXT}"
 
 
-echo "### Subword segmentation with SentencePiece"
-test -s "${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}" ||
-  ${COMPRESSION_CMD} -dc "${corpus_src}" |
-  parallel --no-notice --pipe -k -j "${threads}" --block 50M "${MARIAN}/spm_encode" --model "${vocab_path}" |
-  ${COMPRESSION_CMD} >"${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}"
-test -s "${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}" ||
-  ${COMPRESSION_CMD} -dc "${corpus_trg}" |
-  parallel --no-notice --pipe -k -j "${threads}" --block 50M "${MARIAN}/spm_encode" --model "${vocab_path}" |
-  ${COMPRESSION_CMD} >"${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}"
+echo "### Tokenize the source corpus with SentencePiece."
+  test -s "${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}"                    \
+                                                                        \
+    `# De-compress the source corpus.`                                  \
+    || ${COMPRESSION_CMD} -dc "${corpus_src}"                           \
+                                                                        \
+    `# Parallelize this tokenization.`                                  \
+    | parallel --no-notice --pipe -k -j "${threads}" --block 50M        \
+      `# Tokenize the corpus.`                                         \
+      "${MARIAN}/spm_encode" --model "${vocab_path}"                    \
+                                                                        \
+    `# Save out the final tokenized corpus into a compressed file.`     \
+    | ${COMPRESSION_CMD} >"${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}"
 
-echo "### Creating merged corpus"
-test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" || test -s "${dir}/corpus" ||
-  paste <(${COMPRESSION_CMD} -dc "${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}") <(${COMPRESSION_CMD} -dc "${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}") |
-  sed 's/\t/ ||| /' >"${dir}/corpus"
+echo "### Tokenize the target corpus with SentencePiece."
+  test -s "${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}"                    \
+                                                                        \
+    `# De-compress the target corpus.`                                  \
+    || ${COMPRESSION_CMD} -dc "${corpus_trg}"                           \
+                                                                        \
+    `# Parallelize this tokenization.`                                  \
+    | parallel --no-notice --pipe -k -j "${threads}" --block 50M        \
+      `# Tokenize the corpus.`                                         \
+      "${MARIAN}/spm_encode" --model "${vocab_path}"                    \
+                                                                        \
+    `# Save out the final tokenized corpus into a compressed file.`     \
+    | ${COMPRESSION_CMD} >"${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}"
 
-echo "### Training alignments"
-test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" || test -s "${dir}/align.s2t.${ARTIFACT_EXT}" ||
-  "${BIN}/fast_align" -vod -i "${dir}/corpus" |
-  ${COMPRESSION_CMD} >"${dir}/align.s2t.${ARTIFACT_EXT}"
-test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" || test -s "${dir}/align.t2s.${ARTIFACT_EXT}" ||
-  "${BIN}/fast_align" -vodr -i "${dir}/corpus" |
-  ${COMPRESSION_CMD} >"${dir}/align.t2s.${ARTIFACT_EXT}"
+
+echo "### Merge the source and target corpus into one file"
+  # Only re-generate these files if they don't exist.
+  test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" ||                         \
+  test -s "${dir}/corpus"                            ||                         \
+                                                                                \
+    `# Join the source and target into the same line, using "\t" as delimiter`  \
+    paste <(${COMPRESSION_CMD} -dc "${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}")  \
+          <(${COMPRESSION_CMD} -dc "${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}")  \
+                                                                                \
+    `# Replace the "\" delimiter with the " ||| " delimiter. as fast_align`     \
+    `# requires this as the delimiter`                                          \
+    | sed 's/\t/ ||| /'                                                         \
+                                                                                \
+    `# Output this to the ./corpus file`                                        \
+    > "${dir}/corpus"
+
+echo "### Train the alignments using fast_align - https://github.com/clab/fast_align"
+  # Only re-generate these files if they don't exist.
+  test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" ||                \
+  test -s "${dir}/align.s2t.${ARTIFACT_EXT}"         ||                \
+                                                                       \
+    `# Run fast_align with the "source->target" alignment direction. ` \
+    `# -vod is the recommended default configuration.`                 \
+    "${BIN}/fast_align" -vod -i "${dir}/corpus"                        \
+                                                                       \
+    `# Save out the alignments to a compressed file `                  \
+    | ${COMPRESSION_CMD} >"${dir}/align.s2t.${ARTIFACT_EXT}"
+
+  # Only re-generate these files if they don't exist.
+  test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" ||                              \
+  test -s "${dir}/align.t2s.${ARTIFACT_EXT}"         ||                              \
+                                                                                     \
+    `# Run fast_align with the (reversed -r) "target->source" alignment direction. ` \
+    `# -vod is the recommended default configuration.`                               \
+    "${BIN}/fast_align" -vodr -i "${dir}/corpus"                                     \
+                                                                                     \
+    `# Save out the alignments to a compressed file `                                \
+    | ${COMPRESSION_CMD} >"${dir}/align.t2s.${ARTIFACT_EXT}"
 
 echo "### Symmetrizing alignments"
-test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" || test -s "${dir}/align.t2s" ||
-  ${COMPRESSION_CMD} -d "${dir}/align.s2t.${ARTIFACT_EXT}" "${dir}/align.t2s.${ARTIFACT_EXT}"
-test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" ||
-  "${BIN}/atools" -i "${dir}/align.s2t" -j "${dir}/align.t2s" -c grow-diag-final-and |
-  ${COMPRESSION_CMD} >"${output_dir}/corpus.aln.${ARTIFACT_EXT}"
+  # fast_align generates asymmetric alignments, so the source-target and target-source
+  # alignments can be different. This step symmetrizes the alignments so that they agree.
+  # This will generate a final alignment where neither language is treated as the
+  # "primary" language.
+
+  # Decompress the alignment files.
+  # Only re-generate these files if they don't exist.
+  test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" ||     \
+  test -s "${dir}/align.t2s"                         ||     \
+    `# Decompress the alignment files in place.`            \
+    ${COMPRESSION_CMD} -d                                   \
+      "${dir}/align.s2t.${ARTIFACT_EXT}"                    \
+      "${dir}/align.t2s.${ARTIFACT_EXT}"
+
+  # Only re-generate these files if they don't exist.
+  test -s "${output_dir}/corpus.aln.${ARTIFACT_EXT}" ||                             \
+                                                                                    \
+    `# Use the atools from fast_align to symmetrize the results. This uses the `    \
+    `# default "grow-diag-final-and" heuristic.`                                    \
+    "${BIN}/atools"                                                                 \
+      -i "${dir}/align.s2t"                                                         \
+      -j "${dir}/align.t2s"                                                         \
+      -c grow-diag-final-and                                                        \
+                                                                                    \
+    `# Save out the alignments to a compressed file `                               \
+    | ${COMPRESSION_CMD} >"${output_dir}/corpus.aln.${ARTIFACT_EXT}"
 
 echo "### Creating shortlist"
-test -s "${dir}/lex.s2t.${ARTIFACT_EXT}" ||
-  # extract_lex doesn't support zstd natively; we need to
-  # decrypt first
-  if [ "${ARTIFACT_EXT}" = "zst" ]; then
-    zstdmt -d "${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}"
-    zstdmt -d "${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}"
-    zstdmt -d "${output_dir}/corpus.aln.${ARTIFACT_EXT}"
-    "${BIN}/extract_lex" \
-      "${dir}/corpus.spm.${TRG}" \
-      "${dir}/corpus.spm.${SRC}" \
-      "${output_dir}/corpus.aln" \
-      "${dir}/lex.s2t" \
-      "${dir}/lex.t2s"
-    rm "${dir}/corpus.spm.${TRG}"
-    rm "${dir}/corpus.spm.${SRC}"
-    rm "${output_dir}/corpus.aln"
-  else
-    "${BIN}/extract_lex" \
-      "${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}" \
-      "${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}" \
-      "${output_dir}/corpus.aln.${ARTIFACT_EXT}" \
-      "${dir}/lex.s2t" \
-      "${dir}/lex.t2s"
-  fi
+  # This step generates:
+  # lex.s2t.zst
+  # Only re-generate these files if they don't exist.
+  test -s "${dir}/lex.s2t.${ARTIFACT_EXT}" ||
+    if [ "${ARTIFACT_EXT}" = "zst" ]; then
+      # extract_lex doesn't support zstd natively; we need to decrypt first.
+      zstdmt --rm -d "${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}"
+      zstdmt --rm -d "${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}"
+      zstdmt --rm -d "${output_dir}/corpus.aln.${ARTIFACT_EXT}"
 
-test -s "${dir}/lex.s2t" && ${COMPRESSION_CMD} "${dir}/lex.s2t"
+      "${BIN}/extract_lex"             \
+        "${dir}/corpus.spm.${TRG}"     \
+        "${dir}/corpus.spm.${SRC}"     \
+        "${output_dir}/corpus.aln"     \
+        "${dir}/lex.s2t"               \
+        "${dir}/lex.t2s"
+    else
+      "${BIN}/extract_lex" \
+        "${dir}/corpus.spm.${TRG}.${ARTIFACT_EXT}" \
+        "${dir}/corpus.spm.${SRC}.${ARTIFACT_EXT}" \
+        "${output_dir}/corpus.aln.${ARTIFACT_EXT}" \
+        "${dir}/lex.s2t" \
+        "${dir}/lex.t2s"
+    fi
+
+  # Only export the source->target direction, as it should be symmetrical.
+  test -s "${dir}/lex.s2t" && ${COMPRESSION_CMD} "${dir}/lex.s2t"
 
 echo "### Shortlist pruning"
-test -s "${dir}/vocab.txt" ||
-  "${MARIAN}/spm_export_vocab" --model="${vocab_path}" --output="${dir}/vocab.txt"
-test -s "${output_dir}/lex.s2t.pruned.${ARTIFACT_EXT}" ||
-  ${COMPRESSION_CMD} -dc "${dir}/lex.s2t.${ARTIFACT_EXT}" |
-  grep -v NULL |
-  python3 "prune_shortlist.py" 100 "${dir}/vocab.txt" |
-  ${COMPRESSION_CMD} >"${output_dir}/lex.s2t.pruned.${ARTIFACT_EXT}"
+  # Export the vocab from the SentencePiece model.
+  test -s "${dir}/vocab.txt" ||
+    "${MARIAN}/spm_export_vocab" --model="${vocab_path}" --output="${dir}/vocab.txt"
+
+  test -s "${output_dir}/lex.s2t.pruned.${ARTIFACT_EXT}" ||
+    ${COMPRESSION_CMD} -dc "${dir}/lex.s2t.${ARTIFACT_EXT}" |
+    grep -v NULL |
+    python3 "prune_shortlist.py" 100 "${dir}/vocab.txt" |
+    ${COMPRESSION_CMD} >"${output_dir}/lex.s2t.pruned.${ARTIFACT_EXT}"
 
 echo "### Deleting tmp dir"
 rm -rf "${dir}"
