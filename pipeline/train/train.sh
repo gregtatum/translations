a8d007c7a1b8088a59afb4c03edebff4dcccff2c
Greg Tatum <tatum.creative@gmail.com>
2024-02-26 15:03:16 -0600

    wip-notes-maybe-worth-saving

--- pipeline/train/train.sh
+++ pipeline/train/train.sh
@@ -8,21 +8,33 @@ set -euo pipefail
 
 echo "###### Training a model"
 
-model_type=$1
-training_type=$2
-src=$3
-trg=$4
+model_type                             =student
+training_type                          =train
+src                                    =en
+trg                                    =ca
+train_set_prefixes                     =/home/ubuntu/tasks/task_170792880731767/fetches/corpus
+valid_set_prefix                       =/home/ubuntu/tasks/task_170792880731767/fetches/devset
+model_dir                              =/home/ubuntu/tasks/task_170792880731767/artifacts
+vocab                                  =/home/ubuntu/tasks/task_170792880731767/fetches/vocab.spm
+best_model_metric                      =chrf
+alignments                             =/home/ubuntu/tasks/task_170792880731767/fetches/corpus.aln.zst
+extra_params                           =("${@:11}")
+
+model_type=$1                # student
+training_type=$2             # train
+src=$3                       # en
+trg=$4                       # ca
 # comma separated prefixes to datasets for curriculum learning
 # for example path1/corpus,path2/mono
-train_set_prefixes=$5
-valid_set_prefix=$6
-model_dir=$7
-vocab=$8
-best_model_metric=$9
+train_set_prefixes=$5        # ./fetches/corpus
+valid_set_prefix=$6          # ./fetches/devset
+model_dir=$7                 # ./artifacts
+vocab=$8                     # ./fetches/vocab.spm
+best_model_metric=$9         # chrf
 # comma separated alignment paths that correspond to each training dataset
 # or None to train without alignments
-alignments=${10}
-extra_params=( "${@:11}" )
+x=${10}         # ./fetches/corpus.aln.zst
+extra_params=( "${@:11}" )   # ("${@:11}")
 
 COMPRESSION_CMD="${COMPRESSION_CMD:-pigz}"
 ARTIFACT_EXT="${ARTIFACT_EXT:-gz}"
@@ -45,7 +57,7 @@ cp "configs/opustrainer/${model_type}.yml" "${new_config}"
 # Iterate over the training sets
 # split the input string into an array
 IFS=',' read -ra datasets <<< "${train_set_prefixes}"
-IFS=',' read -ra alns <<< "${alignments}"
+IFS=',' read -ra alignments <<< "${alignments_str}"
 # loop through the array and get both value and index
 for index in "${!datasets[@]}"; do
     train_set_prefix="${datasets[index]}"
@@ -54,14 +66,14 @@ for index in "${!datasets[@]}"; do
     tsv_dataset="${train_set_prefix}.${src}${trg}.tsv" #.gz"
 
     if [ "${alignments}" != "None" ] ; then
-      train_aln="${alns[index]}"
+      train_alignments="${alignments[index]}"
       # alignments are currently used only for student that uses one stage of training
       echo "### Generating tsv dataset with alignments ${alignments}"
       paste <(${COMPRESSION_CMD} -dc "${train_set_prefix}.${src}.${ARTIFACT_EXT}") \
             <(${COMPRESSION_CMD} -dc "${train_set_prefix}.${trg}.${ARTIFACT_EXT}") \
-            <(${COMPRESSION_CMD} -dc "${train_aln}") \
+            <(${COMPRESSION_CMD} -dc "${train_alignments}") \
             >"${tsv_dataset}"
-      rm "${train_aln}"
+      rm "${train_alignments}"
       # when using tsv, marian requires --guided-alignments argument to be an index of the alignments in the tsv file
       extra_params+=("--guided-alignment" "2")
     else
