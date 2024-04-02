1dd1c1145f709876a0a6fdf57c31df8244800e36
Greg Tatum <tatum.creative@gmail.com>
2024-04-02 12:24:52 -0500

    More updates and fixes

--- utils/tasks/serve-docs.sh
+++ utils/tasks/serve-docs.sh
@@ -8,7 +8,7 @@ echo "  rbenv: https://github.com/rbenv/rbenv"
 echo "  rbenv install 3.2.2"
 
 cd docs
-eval "$$(rbenv init - make)"
+eval "$(rbenv init - make)"
 rbenv local 3.2.2
 rbenv shell
 bundle install
