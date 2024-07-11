5516b3a9d5ee5cb3da4d9119e0d6a63c90d0b8a8
Greg Tatum <tatum.creative@gmail.com>
2024-07-11 14:06:53 -0500

    wip

--- convert-marian.sh
+++ convert-marian.sh
@@ -1,7 +1,7 @@
 # Marian ONNX converter.
-marian-conv \
+/builds/worker/tools/marian-dev/build/marian-conv \
   --from       data/onnx-model/model.npz.best-chrf.npz \
   --to         data/onnx-model/model.onnx \
-  --vocabs     data/onnx-model/vocab.enit.spm.gz \
+  --vocabs     data/onnx-model/vocab.enit.spm \
   --gemm-type  float32 \
   --export-as  onnx-encode
